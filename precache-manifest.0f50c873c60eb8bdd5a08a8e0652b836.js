1c71b3ab5562cf5cd12c8f4d76bdeedf9154a677
Theo Dugautier <theo.dugautier@protonmail.com>
2020-03-10 14:26:32 +0100

    initial commit

---
+++ precache-manifest.0f50c873c60eb8bdd5a08a8e0652b836.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9c285f6b2e6a2f505401c683ae9b87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e9812cc14ec37029338",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e3d53631df61d230b0e5",
+    "url": "/static/css/main.d614dcb4.chunk.css"
+  },
+  {
+    "revision": "9e9812cc14ec37029338",
+    "url": "/static/js/2.f2749f04.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.f2749f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d53631df61d230b0e5",
+    "url": "/static/js/main.de633a0d.chunk.js"
+  },
+  {
+    "revision": "67507f0a24b1e3d9b973",
+    "url": "/static/js/runtime-main.b21d1ac0.js"
+  },
+  {
+    "revision": "c02e267a6475bdf54c8436961564ba56",
+    "url": "/static/media/images-test.c02e267a.jpg"
+  }
+]);
